a096200c2b3b829d8286d5c0bc647866025bcbc6
Jack Mkimbo <nickihll@gmail.com>
2021-02-19 18:35:20 +0300

    adding CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-twitter`,
+    'gatsby-plugin-netlify-cms',
     `gatsby-plugin-catch-links`,
     {
       resolve: 'gatsby-plugin-sitemap',
